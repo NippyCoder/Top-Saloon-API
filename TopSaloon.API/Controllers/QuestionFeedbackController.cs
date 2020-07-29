207c231d21f8b9b8e700e56910d2f91bf95c9718
Ahmed Elbandrawy <a.elbandrawy@boltss.com>
2020-07-29 17:31:52 +0200

    commit

--- TopSaloon.API/Controllers/QuestionFeedbackController.cs
+++ TopSaloon.API/Controllers/QuestionFeedbackController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TopSalon.DTOs.Models;
 using TopSaloon.API.Controllers.Common;
 using TopSaloon.DTOs.Models;
 using TopSaloon.ServiceLayer;
