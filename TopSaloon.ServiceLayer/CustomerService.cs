621227ddfa0193f44b631d6b7ea9552777486dc4
amr moushtaha <amr.moushtaha@boltss.com>
2020-07-29 13:01:14 +0200

    Updated services

--- TopSaloon.ServiceLayer/CustomerService.cs
+++ TopSaloon.ServiceLayer/CustomerService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using TopSalon.DTOs.Models;
 using TopSaloon.Core;
 using TopSaloon.DTOs;
 using TopSaloon.DTOs.Enums;
@@ -102,5 +103,68 @@ namespace TopSaloon.ServiceLayer
                 return result;
             }
         }
+
+        public async Task<ApiResponse<int>> GetCustomerTotalNumberOfVisit(int UserId)
+        {
+            ApiResponse<int> result = new ApiResponse<int>();
+
+            try
+            {
+                Customer customer = await unitOfWork.CustomersManager.GetByIdAsync(UserId);
+
+                if (customer != null)
+                {
+                    result.Data = (int)customer.TotalNumberOfVisits;
+                    result.Succeeded = true;
+                    return result;
+                }
+                result.Succeeded = false;
+                result.Errors.Add("It Is New Customer !");
+                result.ErrorType = ErrorType.LogicalError;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result.Succeeded = false;
+                result.Errors.Add(ex.Message);
+                return result;
+            }
+        }
+
+        public async Task<ApiResponse<CustomerDetailsModelDTO>> GetCustomerVisitDetails(int UserId)
+        {
+            ApiResponse<CustomerDetailsModelDTO> result = new ApiResponse<CustomerDetailsModelDTO>();
+
+            try
+            {
+                Customer customer = await unitOfWork.CustomersManager.GetByIdAsync(UserId);
+
+                if (customer != null)
+                {
+                    CustomerDetailsModelDTO customerDetailsModelDTO = new CustomerDetailsModelDTO();
+                    customerDetailsModelDTO.Name = customer.Name;
+                    customerDetailsModelDTO.PhoneNumber = customer.PhoneNumber;
+                    customerDetailsModelDTO.TotalNumberOfVisit = (int)customer.TotalNumberOfVisits;
+                    customerDetailsModelDTO.dateVisitDate = (DateTime)customer.LastVisitDate;
+
+
+
+                    result.Data = customerDetailsModelDTO;
+                    result.Succeeded = true;
+                    return result;
+                }
+                result.Succeeded = false;
+                result.Errors.Add("It Is New Customer !");
+                result.ErrorType = ErrorType.LogicalError;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result.Succeeded = false;
+                result.Errors.Add(ex.Message);
+                return result;
+            }
+        }
+
     }
 }
