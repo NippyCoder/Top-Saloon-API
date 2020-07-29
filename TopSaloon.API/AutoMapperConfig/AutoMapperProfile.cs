8a97dd4c25d7a4b2d16f2ba8a89c189e3201eb03
amr moushtaha <amr.moushtaha@boltss.com>
2020-07-29 13:49:03 +0200

    Added AutoMapper

---
+++ TopSaloon.API/AutoMapperConfig/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopSaloon.API.AutoMapperConfig
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+           // CreateMap<TestEntity, TestEntityDTO>().ReverseMap();
+         
+        }
+    }
+}
