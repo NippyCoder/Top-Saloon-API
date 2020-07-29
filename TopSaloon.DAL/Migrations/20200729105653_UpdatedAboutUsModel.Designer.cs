2c33b66248a12ccd08c3c098fefd93a5f0d18ce5
amr moushtaha <amr.moushtaha@boltss.com>
2020-07-29 12:57:46 +0200

    Updated Models

--- TopSaloon.DAL/Migrations/20200729105653_UpdatedAboutUsModel.Designer.cs
+++ TopSaloon.DAL/Migrations/20200729105653_UpdatedAboutUsModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TopSaloon.DAL;
 
 namespace TopSaloon.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200729105653_UpdatedAboutUsModel")]
+    partial class UpdatedAboutUsModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
