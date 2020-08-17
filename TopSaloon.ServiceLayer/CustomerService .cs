2ad4ef48e87a8977a72a0217e5831869bd7f5caa
Ahmed Elbandrawy <a.elbandrawy@boltss.com>
2020-08-17 14:40:57 +0200

    commit changes

--- TopSaloon.ServiceLayer/CustomerService .cs
+++ TopSaloon.ServiceLayer/CustomerService .cs
@@ -150,7 +150,7 @@ namespace TopSaloon.ServiceLayer
                     result.Data = mapper.Map<CustomerDTO>(customer[0]);
                     result.Succeeded = true;
                     return result;
-                }
+                }   
                 else
                 {
                     result.Succeeded = false;
