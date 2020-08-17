dba5ceecabb257385a587bf5ad2218d3c504bb12
Nader Hosny <n.hosny@boltss.com>
2020-08-17 14:37:34 +0200

    - Included 'BarberProfilePhoto' with Available barbers function, - Included CORS for startup.

--- TopSaloon.API/Startup.cs
+++ TopSaloon.API/Startup.cs
@@ -45,6 +45,13 @@ namespace TopSaloon.API
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddUserManager<ApplicationUserManager>();
 
+            services.AddCors(options =>
+                options.AddDefaultPolicy(builder =>
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()));
+
             services.AddAutoMapper(typeof(AutoMapperProfile));
 
             services.AddScoped<UnitOfWork>();
@@ -73,6 +80,11 @@ namespace TopSaloon.API
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
 
+            app.UseCors(policy => policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://localhost:4200")
+            .AllowCredentials());
 
             app.UseRouting();
 
